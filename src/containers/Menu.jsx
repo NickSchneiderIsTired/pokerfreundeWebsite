a120e7761c3a871a7825562f892da5695573b280
Nick Schneider <nick-s2000@web.de>
2023-11-13 15:02:06 +0100

    Improve menu

--- src/containers/Menu.jsx
+++ src/containers/Menu.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import '../App.css';
+import { useNavigate } from 'react-router';
+import { Button } from '@mui/material';
 
 function Menu() {
+  const navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome!</h1>
-      </header>
+      <div className="App-header">
+        <div className='content'>
+          <h1>Pokerfreunde 2018 e.V.</h1>
+          <p>
+            Pokern im Verein ohne Geldeinsätze
+          </p>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Button
+              onClick={() => navigate("/register")}
+              variant='contained'
+            >
+              Turnieranmeldung
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
