aec429ea94447753d43355713128908aff4294a8
Nick Schneider <nick-s2000@web.de>
2023-11-13 15:02:42 +0100

    Improve registry

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -7,7 +7,7 @@ function Register() {
       <div className="App-header">
         <div className='content'>
           <h1>Dieser Seite ist aktuell im Umbau</h1>
-          Anmeldungen bitte direkt per E-Mail an 
+            Anmeldungen bitte direkt per E-Mail mit Namen an 
           <a
             className="App-link"
             style={{ marginLeft: 6 }} href="mailto:pokerfreunde2023@gmail.com">pokerfreunde2023@gmail.com</a>
