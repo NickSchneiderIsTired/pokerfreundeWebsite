3de360b1d2febda5cb16ac065620f1d5ec024e68
Nick Schneider <nick-s2000@web.de>
2024-02-05 21:23:59 +0100

    Update ranking file paths

--- src/containers/Rankings.jsx
+++ src/containers/Rankings.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import '../App.css';
 import { Button, Grid } from '@mui/material';
 import { PictureAsPdf } from '@mui/icons-material';
+import MTT from '../res/MTT.pdf'
+import SnG from '../res/SnG.pdf'
 
 function Rankings() {
   return (
@@ -16,7 +18,8 @@ function Rankings() {
                 endIcon={<PictureAsPdf style={{ color: 'black' }} color="inherit"/>}
                 variant='contained'
                 target="_blank"
-                href={process.env.PUBLIC_URL + '/MTT.pdf'}
+                href={MTT}
+                download={"MTT.pdf"}
                 style={{ color: 'black' }}
               >
                 MTT
@@ -27,7 +30,8 @@ function Rankings() {
                 endIcon={<PictureAsPdf style={{ color: 'black' }} color="inherit"/>}
                 variant='contained'
                 target="_blank"
-                href={process.env.PUBLIC_URL + '/SnG.pdf'}
+                href={SnG}
+                download={"SnG.pdf"}
                 style={{ color: 'black' }}
               >
                 SnG
