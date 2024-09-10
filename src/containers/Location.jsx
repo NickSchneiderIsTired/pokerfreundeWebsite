fb507422fd014ef0865bbbb7a83faac04aecb869
Stefan Akie <stefan.akie@grommunio.com>
2024-09-10 10:43:29 +0200

    Update location

--- src/containers/Location.jsx
+++ src/containers/Location.jsx
@@ -7,13 +7,13 @@ function Location() {
       <div className="App-header">
         <div className='content'>
           <h1>Location</h1>
-          <p>Unser Vereinsheim befindet sich im Landgasthof Nassenbeuren</p>
+          <p>Unser Vereinsheim befindet sich im Schlossgarten Türkheim</p>
           <div className='iframeContainer'>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10662.402080480242!2d10.5201559!3d48.0793533!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479c0588f385e1eb%3A0xac6f9c49c449923e!2sLandgasthof%20Nassenbeuren!5e0!3m2!1sde!2sde!4v1699883632402!5m2!1sde!2sde"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28476.932074070268!2d10.618827528239686!3d48.05814194103948!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479c1084709b0773%3A0x148c80075ce02fdf!2sSchlossgarten!5e0!3m2!1sde!2sde!4v1725957744344!5m2!1sde!2sde"
               width="100%"
               height="450"
-              allowfullscreen=""
+              allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade">
             </iframe>
